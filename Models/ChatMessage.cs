b18e58dfa28956f57877676f6e92f58a05802fcd
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-08-23 14:37:44 -0700

    add ChatMessage model Co-authored-by: MaxwellMeyer <MaxwellMeyer@users.noreply.github.com> Co-authored-by: Allister <AlyxMoon@users.noreply.github.com>

--- Models/ChatMessage.cs
+++ Models/ChatMessage.cs
@@ -1,22 +1,13 @@
-// using System.Collections.Generic;
-// using System.ComponentModel.DataAnnotations;
-// using System.ComponentModel.DataAnnotations.Schema;
-
 namespace LeekQuest.Models
 {
   public class ChatMessage
   {
-    // public Flavor()
-    // {
-    //   this.JoinEntities = new HashSet<FlavorTreat>();
-    // }
 
     public int ChatMessageId { get; set; }
     public string UserId { get; set; }
     public string Message { get; set; }
     public string TimeStamp { get; set; }
     public int NumLikes { get; set; }
-    public virtual ApplicationUser User { get; set; }
-    // public virtual ICollection<FlavorTreat> JoinEntities { get; set; }
+    public virtual User User { get; set; }
   }
 }
