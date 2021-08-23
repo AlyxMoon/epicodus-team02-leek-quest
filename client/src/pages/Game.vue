aaaa31754916975cb75b14fafdd8bbf00ec9258f
AlyxMoon <allisterkmoon@gmail.com>
2021-08-23 14:43:08 -0700

    add basic content/styling to show a grid

--- client/src/pages/Game.vue
+++ client/src/pages/Game.vue
@@ -1,14 +1,46 @@
-<template>
-  <h1>Page!</h1>
-  <p>This is the game board! click on a tile to move your leek!</p>
+<template>  
+  <div class="grid">
+    <div 
+      class="cell" 
+      v-for="i of (boardSize * boardSize)"
+      :key="i"
+    >
+      {{ i }}
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'GamePage',
+  data: () => ({
+    boardSize: 20,
+  }),
 }
 </script>
 
 <style scoped>
 
+.grid {
+  --cell-size: 30px;
+
+  display: grid;
+  grid-gap: 1px;
+
+  background-color: black;
+  border: 1px solid black;
+
+  margin: 0 auto;
+  max-width: fit-content;
+
+  grid-template-columns: repeat(20, var(--cell-size));
+  grid-auto-rows: var(--cell-size);
+}
+
+.cell {
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
