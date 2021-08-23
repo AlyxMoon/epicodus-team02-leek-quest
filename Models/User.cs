a14cad1647a79c2b6b8deff92731e6b41b3e5d2f
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-08-23 14:37:15 -0700

    add User model Co-authored-by: MaxwellMeyer <MaxwellMeyer@users.noreply.github.com> Co-authored-by: Allister <AlyxMoon@users.noreply.github.com>

--- Models/User.cs
+++ Models/User.cs
@@ -1,24 +1,16 @@
-// is this basically just ApplicationUser.cs???
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
 
-// // using System.Collections.Generic;
-// // using System.ComponentModel.DataAnnotations;
-// // using System.ComponentModel.DataAnnotations.Schema;
-
-// namespace LeekQuest.Models
-// {
-//   public class User
-//   {
-//     // public Flavor()
-//     // {
-//     //   this.JoinEntities = new HashSet<FlavorTreat>();
-//     // }
-
-//     public int UserId { get; set; }
-//     public string Username { get; set; }
-//     public string Password { get; set; }
-//     public string Position { get; set; }
-//     public int NumLikes { get; set; }
-//     public virtual ApplicationUser User { get; set; }
-//     // public virtual ICollection<FlavorTreat> JoinEntities { get; set; }
-//   }
-// }
+namespace LeekQuest.Models
+{
+  public class User : IdentityUser
+  {
+    public User()
+    {
+      this.ChatMessages = new HashSet<ChatMessage>();
+    }
+    public string Position { get; set; }
+    public int NumLikes { get; set; }
+    public virtual ICollection<ChatMessage> ChatMessages { get; set; }
+  }
+}
